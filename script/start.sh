18585c461cb9349ea6d01570517b0b68addf8892
Paweł Obrok <pawel@blockfi.com>
2020-10-16 14:18:19 +0200

    Use exec

--- script/start.sh
+++ script/start.sh
@@ -1,4 +1,4 @@
 HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 | sed "s/\./-/g")
 export RELEASE_NODE=cluster_test@ip-${HOSTNAME}
 
-/cluster_test/bin/cluster_test start
+exec /cluster_test/bin/cluster_test start
