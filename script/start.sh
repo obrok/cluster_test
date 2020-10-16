bef43d367fa711348365d8a5a20d5d011ef88b75
Paweł Obrok <pawel@blockfi.com>
2020-10-16 12:54:50 +0200

    Use hostname instead of ip

--- script/start.sh
+++ script/start.sh
@@ -1,4 +1,4 @@
-HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-ipv4n/)
+HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
 export RELEASE_NODE=cluster_test@${HOSTNAME}
 
 /cluster_test/bin/cluster_test start
