27a9cef22cd5eb5306d8f44b13b12af7675fcec0
Paweł Obrok <pawel@blockfi.com>
2020-10-16 13:09:42 +0200

    Use short hostname

--- script/start.sh
+++ script/start.sh
@@ -1,4 +1,4 @@
-HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
-export RELEASE_NODE=cluster_test@${HOSTNAME}
+HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname | sed "s/\./-/g")
+export RELEASE_NODE=cluster_test@ip-${HOSTNAME}
 
 /cluster_test/bin/cluster_test start
