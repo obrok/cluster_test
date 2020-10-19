f77df1239a3680157e933df5a0bc262c32ca7d99
Paweł Obrok <pawel@blockfi.com>
2020-10-19 15:14:17 +0200

    Add a global worker

--- lib/cluster_test.ex
+++ lib/cluster_test.ex
@@ -1,15 +1,25 @@
 defmodule ClusterTest do
-  defmodule Checker do
-    use Task
+  defmodule Reporter do
+    require Logger
 
-    def start_link(_), do: Task.start_link(__MODULE__, :run, [])
+    use GenServer
 
-    def run() do
+    def start_link(name) do
+      Util.start_link_maybe_global(fn ->
+        GenServer.start_link(__MODULE__, name, name: name)
+      end)
+    end
+
+    def init(name) do
+      :timer.send_interval(:timer.seconds(1), :report)
+      {:ok, name}
+    end
+
+    def handle_info(:report, name) do
       require Logger
 
-      Logger.info("Connected to: #{inspect(:erlang.nodes())}")
-      :timer.sleep(1000)
-      run()
+      Logger.info("Reporting #{inspect(name)}")
+      {:noreply, name}
     end
   end
 
@@ -29,10 +39,31 @@ defmodule ClusterTest do
 
     children = [
       {Cluster.Supervisor, [topologies, [name: ClusterTest.ClusterSupervisor]]},
-      __MODULE__.Checker
+      Supervisor.child_spec({__MODULE__.Reporter, __MODULE__.Reporter}, id: :local_reporter),
+      Supervisor.child_spec({__MODULE__.Reporter, {:global, __MODULE__.Reporter}}, id: :global_reporter)
     ]
 
     opts = [strategy: :one_for_one, name: __MODULE__]
     Supervisor.start_link(children, opts)
+  end
+
+  defmodule Util do
+    def start_link_maybe_global(fun) do
+      case fun.() do
+        {:error, {:already_started, pid}} ->
+          Task.start_link(fn ->
+            ref = Process.monitor(pid)
+
+            receive do
+              {:DOWN, ^ref, :process, _, _} ->
+                Logger.warn("Lost connection to #{inspect(name)}")
+                :stop
+            end
+          end)
+
+        other ->
+          other
+      end
+    end
   end
 end
